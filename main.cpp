f83d07968ec9525feb1213327f4eef9207586763
THNTS <daniilbi99@gmail.com>
2018-11-25 20:48:20 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    for(int i = 0; i < atoi(argv[1]); i++)
+    {
+        cout << "Path Planning" << endl;
+    }
+    return 0;
+}
